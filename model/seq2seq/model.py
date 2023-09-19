d9fd18b492a889bddd309875121b0a00dbfee41b
Jose Sanchez <37961431+JosephS96@users.noreply.github.com>
2023-09-18 19:20:02 -0600

    Started outline for encoder architecture

---
+++ model/seq2seq/model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, text_processor, units):
+        super(Encoder, self).__init__()
+        self.text_processor = text_processor
+        self.vocab_size = text_processor.vocabulary_size()
+        self.units = units
+
+        # The embedding layer converts tokens to vectors
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, units, mask_zero=True)
+
+        # The RNN layer processes those vectors sequentially.
+        self.rnn = tf.keras.layers.Bidirectional(
+            merge_mode='sum',
+            layer=tf.keras.layers.GRU(
+                units,
+                return_sequences=True,
+                recurrent_initializer='glorot_uniform'
+            )
+        )
+
+    def call(self, x):
+        pass
+
+    def convert_input(self, texts):
+        pass
