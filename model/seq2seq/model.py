940560b6e7d0f15d64c4c208adc4c79f6212a3f2
Jose Sanchez <37961431+JosephS96@users.noreply.github.com>
2023-10-05 00:14:48 -0600

    Created seq2seq model with attention

--- model/seq2seq/model.py
+++ model/seq2seq/model.py
@@ -1,6 +1,39 @@
 import tensorflow as tf
 
 
+class Translator(tf.keras.Model):
+    @classmethod
+    def add_method(cls, fun):
+        setattr(cls, fun.__name__, fun)
+        return fun
+
+    def __init__(self,
+                 units,
+                 context_text_processor,
+                 target_text_processor
+                 ):
+        super.__init__()
+
+        # Build encoder and decoder
+        encoder = Encoder(context_text_processor, units)
+        decoder = Decoder(target_text_processor, units)
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def call(self, inputs):
+        context, x = inputs
+        context = self.encoder(context)
+        logits = self.decoder(context, x)
+
+        try:
+            # Delete the keras mask, so keras doesn't scale the loss+accuracy.
+            del logits._keras_mask
+        except AttributeError:
+            pass
+
+        return logits
+
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, text_processor, units):
         super(Encoder, self).__init__()
@@ -22,7 +55,82 @@ class Encoder(tf.keras.layers.Layer):
         )
 
     def call(self, x):
-        pass
+        x = self.embedding(x)
+        x = self.rnn(x)
+
+        return x
 
     def convert_input(self, texts):
-        pass
+        texts = tf.convert_to_tensor(texts)
+        if len(texts.shape) == 0:
+            texts = tf.convert_to_tensor(texts)[tf.newaxis]
+        context = self.text_processor(texts).to_tensor()
+        context = self(context)
+
+        return context
+
+class CrossAttention(tf.keras.layers.Layer):
+    def __init__(self, units, **kwargs):
+        super.__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(key_dim=units, num_heads=1, **kwargs)
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+    def call(self, x, context):
+        attn_output, attn_scores = self.mha(
+            query=x,
+            value=context,
+            return_attention_scores=True
+        )
+
+        # Cache attention scores for plotting
+        attn_scores = tf.reduce_mean(attn_scores, axis=1)
+        self.last_attention_weights = attn_scores
+
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+
+        return x
+
+class Decoder(tf.keras.layers.Layer):
+    @classmethod
+    def add_method(cls, fun):
+        setattr(cls, fun.__name__, fun)
+        return fun
+
+    def __init__(self, text_processor, units):
+        super(Decoder, self).__init__()
+        self.text_processor = text_processor
+        self.vocab_size = text_processor.vocabulary_size()
+        self.word_to_id = tf.keras.layers.StringLookup(
+            vocabulary=text_processor.get_vocabulary(),
+            mask_token='',
+            oov_token='[UNK]'
+        )
+        self.id_to_word = tf.keras.layers.StringLookup(
+            vocabulary=text_processor.get_vocabulary(),
+            mask_token='',
+            oov_token='[UNK]',
+            invert=True
+        )
+        self.start_token = self.word_to_id('[START]')
+        self.end_token = self.word_to_id('[END]')
+
+        self.units = units
+
+        # The embedding layer converts token IDs to vectors
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, units, mask_zero=True)
+
+        # The RNN keep tracks of what's been generated so far
+        self.rnn = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+
+        # The RNN output will be the query for the attention layer
+        self.attention = CrossAttention(units)
+
+        # The fully connected layer produces the logits for each output token
+        self.output_layer = tf.keras.layers.Dense(self.vocab_size)
